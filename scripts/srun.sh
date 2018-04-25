f7120a24aa1598f4c4ef5e3efedddb5b71aeba34
John Molina <john@cheme.kyoto-u.ac.jp>
2018-04-25 14:12:15 +0900

    Update scripts to include silo path

--- scripts/srun.sh
+++ scripts/srun.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #QSUB -q  gr10331b
 #QSUB -ug gr10331
-#QSUB -W 12:0
-#QSUB -oo std.out
-#QSUB -eo std.err
-#QSUB -M john@cheme.kyoto-u.ac.jp
-#QSUB -A p=1:t=1:c=1:m=2G
+#QSUB -W  0:10
+#QSUB -o  std.out
+#QSUB -e  std.err
+#QSUB -M  john@cheme.kyoto-u.ac.jp
+#QSUB -A  p=1:t=1:c=1:m=2G
 #QSUB -ug gr10331
-#QSUB -m be
+#QSUB -m  be
 #================ PBS Options ====================
 
 #==================Shell Script===================
 cd $QSUB_WORKDIR
 set -x
+export LD_LIBRARY_PATH=/LARGE0/gr10331/app/silo/4.10.2/intel-17.0/lib:$LD_LIBRARY_PATH
 
 # automatically
 ./diffusion2d.sopt
